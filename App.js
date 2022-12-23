134eaacd5e09638dcb86ad20857ff70a232e3c42
vanshikaG <vanshikagautam22@gmail.com>
2022-12-23 10:37:22 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+//import Header from "./components/Header"
+//import Meme from "./components/Meme"
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return null;
+    /*(
+        <div>
+            <Header/>
+            <Meme />
+        </div>
+    )*/
+}
---
+++ components/Meme.js
@@ -0,0 +1,33 @@
+import React from "react"
+import memesData from "../memesData"
+
+export default function Meme(){
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        console.log(randomNumber)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+    return(
+        <main>
+            <div className="div">
+                <input 
+                type="text" 
+                placeholder="Top text" 
+                className="form--input"  
+                />
+                <input 
+                type="text" 
+                placeholder="Bottom text" 
+                className="form--input" />
+                <button 
+                className="form--button"
+                onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼</button>
+            </div>
+        </main>
+    )
+}
